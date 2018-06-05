2ec9a4012c9d5177f6dbeff74b0a36691a3ae2e9
Jestemkioskiem <Dlephhi@gmail.com>
2018-06-05 21:21:38 +0200

    Move time_limit variable down again.

--- raffle_bot/app.py
+++ raffle_bot/app.py
@@ -35,9 +35,9 @@ async def command(message,text):
 async def run_raffle(message, time_limit, prize):
 	if time_limit >= config.settings['min_time'] and time_limit <= config.settings['max_time']: # Default min and max time for a raffle. Replace if necessary. 
 		time_remaining = int(time_limit/10)
-		time_limit *= 60 # Converting from seconds to minutes.
-
 		initial_message = await client.send_message(message.channel, config.r_messages['raffle_start'].format(message.author.id, time_limit, prize))
+		
+		time_limit *= 60 # Converting from seconds to minutes.
 		await asyncio.sleep(time_limit - time_remaining)
 		
 		await client.send_message(message.channel, config.r_messages['raffle_ending'].format(time_remaining, message.author.id))
